a272d1edf9c8498bf90e2741870b781711a5b994
8ear <maximilian.haeckl@dcso.de>
2018-04-03 17:06:18 +0200

    update misp-server container

--- container/misp-server/2.4.89/configuration.sh
+++ container/misp-server/2.4.89/configuration.sh
@@ -1,8 +1,11 @@
 # 
+# Container Name
+CONTAINER_NAME="misp-server"
 # HTTPS GIT Repo URL:
 GIT_REPO=https://github.com/DCSO/MISP-dockerized
 # MISP TAG to build:
 MISP_TAG=2.4.89
+VERSION="$MISP_TAG-ubuntu"
 # MISP Dependencies:
 python_cybox_TAG=v2.1.0.12
 python_stix_TAG=v1.1.1.4
--- container/misp-server/build.sh
+++ container/misp-server/build.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -x
-[ -z $1 ] && echo "please get Version as parameter." && exit 1
-VERSION="$1"
 
-CONTAINER_NAME="misp-server"
+# check if at least one parameter exists
+[ -z $1 ] && echo "please get Version as parameter." && exit 1
+FOLDER="$1"
+
+source $FOLDER/configuration.sh
 
 DOCKER_REPO="dcso/$CONTAINER_NAME"
 IMAGE_NAME="$DOCKER_REPO:latest"
 DOCKERFILE_PATH=Dockerfile
 
-pushd $VERSION
-source ./configuration.sh
-
 sudo docker build \
+        --build-arg RELEASE_DATE="$(date +"%Y-%m-%d")" \
         --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-        --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+        --build-arg NAME="$CONTAINER_NAME" \
         --build-arg GIT_REPO="$GIT_REPO" \
+        --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+        --build-arg VERSION="$VERSION" \
         --build-arg MISP_TAG="$MISP_TAG" \
         --build-arg python_cybox_TAG="$python_cybox_TAG" \
         --build-arg python_stix_TAG="$python_stix_TAG" \
         --build-arg mixbox_TAG="$mixbox_TAG" \
         --build-arg cake_resque_TAG="$cake_resque_TAG" \
-    -f $DOCKERFILE_PATH -t $IMAGE_NAME -t $DOCKER_REPO:$MISP_TAG .
-
-popd
+    -f $FOLDER/$DOCKERFILE_PATH -t $IMAGE_NAME -t $DOCKER_REPO:$VERSION $FOLDER/
 
 # ##################################################
 # # for documentation:
