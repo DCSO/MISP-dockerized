daa73eb52a310d1d25bd53e29730a637201cf248
8ear <maximilian.haeckl@dcso.de>
2018-04-03 17:47:15 +0200

    update misp-proxy

--- container/misp-proxy/1.0.0-alpine/configuration.sh
+++ container/misp-proxy/1.0.0-alpine/configuration.sh
@@ -5,12 +5,3 @@ CONTAINER_NAME="misp-proxy"
 GIT_REPO=https://github.com/DCSO/MISP-dockerized
 # Version:
 VERSION=1.0.0
-# Automatic Docker Variable:
-    DOCKER_REPO="dcso/$CONTAINER_NAME"
-    IMAGE_NAME="$DOCKER_REPO:latest"
-    DOCKERFILE_PATH=Dockerfile
-
-# Tags
-ADDITIONAL_TAGS=   "-t $DOCKER_REPO:alpine \
-                    -t $DOCKER_REPO:$VERSION-alpine \
-                    -t $DOCKER_REPO:$VERSION-latest"
