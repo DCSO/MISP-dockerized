1a1037ecdf5f993202f6f220f73535c0d4032cab
M. Haeckl <maximilian.haeckl@dcso.de>
2018-04-03 17:09:27 +0200

    Rc.1 (#5)
    * update misp-proxy container
    * update misp-server container
    * update misp-robot container and mv all scripts to misp-robot if possible

---
+++ container/misp-proxy/1.0.0-ubuntu/configuration.sh
@@ -0,0 +1,17 @@
+# 
+# Container Name
+CONTAINER_NAME="misp-proxy"
+# HTTPS GIT Repo URL:
+GIT_REPO=https://github.com/DCSO/MISP-dockerized
+# Version:
+VERSION=1.0.0
+# Automatic Docker Variable:
+    DOCKER_REPO="dcso/$CONTAINER_NAME"
+    IMAGE_NAME="$DOCKER_REPO:ubuntu"
+    DOCKERFILE_PATH=Dockerfile
+
+# Tags
+ADDITIONAL_TAGS=""
+# ADDITIONAL_TAGS=   "-t $DOCKER_REPO:alpine \
+#                     -t $DOCKER_REPO:$VERSION-alpine \
+#                     -t $DOCKER_REPO:$VERSION-latest"
--- container/misp-proxy/build.sh
+++ container/misp-proxy/build.sh
@@ -18,12 +18,7 @@ sudo docker build \
         --build-arg GIT_REPO="$GIT_REPO" \
         --build-arg VCS_REF=$(git rev-parse --short HEAD) \
         --build-arg VERSION="$VERSION" \
-        --build-arg MISP_TAG="$MISP_TAG" \
-        --build-arg python_cybox_TAG="$python_cybox_TAG" \
-        --build-arg python_stix_TAG="$python_stix_TAG" \
-        --build-arg mixbox_TAG="$mixbox_TAG" \
-        --build-arg cake_resque_TAG="$cake_resque_TAG" \
-    -f $FOLDER/$DOCKERFILE_PATH -t $IMAGE_NAME -t $DOCKER_REPO:$VERSION $FOLDER/
+    -f $FOLDER/$DOCKERFILE_PATH -t $IMAGE_NAME -t $DOCKER_REPO:$FOLDER $FOLDER/
 
 # ##################################################
 # # for documentation:
