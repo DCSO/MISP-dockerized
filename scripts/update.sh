f2e91c2301e54fc398116458569ede2a44408904
8ear <github@8ear.de>
2018-06-19 08:49:18 +0200

    Update update script, but its not finished

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,7 +10,7 @@ MISP_dockerized_path="$SCRIPTPATH/../misp-dockerized/"
 # check if required components exists
 function check_components(){
   echo -n "Check requirements..."
-  for bin in docker-compose docker git awk sha1sum; do
+  for bin in $@; do
     if [[ -z $(which ${bin}) ]]; then echo "Cannot find ${bin}, exiting..."; exit 1; fi
   done
   # check curl
@@ -25,6 +25,43 @@ function check_components(){
   if grep --help 2>&1 | head -n 1 | grep -q -i "busybox"; then echo "BusybBox grep detected, please install gnu grep, \"apk add --no-cache --upgrade grep\""; exit 1; fi
   if cp --help 2>&1 | head -n 1 | grep -q -i "busybox"; then echo "BusybBox cp detected, please install coreutils, \"apk add --no-cache --upgrade coreutils\""; exit 1; fi
   echo "Finished."
+}
+
+function current_tag(){
+
+}
+
+# Function: To which Tag should we update?
+function update_2_tag(){
+  search_myTag
+  echo -n "Check if update is possible..."
+  # Set Var
+  i=1
+  # search all TAGS newer then the current one
+  for TAG in $TAGS; do
+      CURRENT_TAG_TIMESTAMP="$(git log $TAG --format="%ct"|head -1)"
+      if [ $CURRENT_TAG_TIMESTAMP > $myTAG_TIMESTAMP ]; then continue;fi; # only show newer release tags!
+      #echo "[ ${i} ] - ${TAG}"
+      TAG_SELECTION[${i}]="${TAG}"
+      ((i++))
+  done
+  # check if Update is possible!
+  if [ ${#TAG_SELECTION[@]} = 0 ]; then echo -e "Finished. No Update available. I will exit.\n";exit 1;fi;
+  echo
+  # List all Tags as parameter
+  echo "The following Tags are available for you:"
+  for i in ${#TAG_SELECTION[@]}
+  do
+    echo "[ ${i} ] - ${TAG_SELECTION[$i]}"
+  done
+  echo
+  # set and check input var
+  input_sel=0
+  while [[ ${input_sel} -lt 1 ||  ${input_sel} -gt ${i} ]]; do
+    read -p "Select a Tag to Update: " input_sel
+  done
+  # set new Tag
+  NEW_TAG="${TAG_SELECTION[${input_sel}]}"
 }
 
 #
@@ -71,7 +108,10 @@ echo "############### Starting Update Script  #################"
 echo "# Description: This Script update the MISP Environment. #"
 echo "#########################################################"
 
-check_components  # check if all binaries existing
+# check if all binaries existing
+check_components docker git awk sha1sum 
+# what is your current tag
+current_tag
 # To which tag should we update?
 update_2_tag      
 # what we need to do for the Update:
