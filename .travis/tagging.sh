3c88d223dbfd8bae2f670dc417c8d56ecf3cf9eb
8ear <github@8ear.de>
2018-09-08 00:39:18 +0200

    Bugfixing fro gitlab-ci

--- .travis/tagging.sh
+++ .travis/tagging.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
-
+set -exv
 
 DOCKER_REPO="$1"
 tag="$2"
+[ -z $(echo $tag|grep dev) ] && tag="$tag-dev"
 image_id=$(docker images --format "{{.Repository}}:{{.Tag}}:{{.ID}}"|grep $DOCKER_REPO:$tag|cut -d : -f 3|head -n 1;)
 image_tags=$(docker images --format "{{.Repository}}:{{.Tag}}:{{.ID}}"|grep $image_id|cut -d : -f 2;)
 for i in $image_tags
