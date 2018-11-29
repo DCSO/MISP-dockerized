c184a4e05bda229d44b3becd9a5983c704c90ec0
Max H <10329648+8ear@users.noreply.github.com>
2018-11-29 09:22:39 +0100

    Feat.MDD129 - MISP-dockerized release from beta to production 1.0.0 (#43)
    ## Feat.MDD129 - MISP-dockerized release from beta to production 1.0.0
    ### Update Informations
    Now we release the version 1.0.0. This is the first production ready release.
    ### General Changes
    ### Detailed Changes
    #### Main Repository
    ##### Detailed Changes, Fixes & Improvements
    - The postfix is now installed at misp-server. Because MISP application sends mail via phpmailer and phpmailer require a local mail program to send mails. Therefore we asked us why we require a second mail container? The answer was we doesn't require a second mail container.
    - We changed also the version name scheme for the container from three (3) digits to two (2) digits. Also we introduced a new major tag for the docker container. Therefore if you do an update every week you get only minor changes in the container. For example a different cipher suite or a fixed bug. The major changes are for example a switch from ubuntu to debian or any else big change. 
    - We introduced therefore also update and upgrade scripts. For minor update and major upgrade.
    - We changed also our naming scheme in the main repository to get a state with all version, also the older one.
    #### Container `misp-dockerized-server`
    ##### General/Update Informations
    ##### Detailed Changes, Fixes & Improvements
    #### Container `misp-dockerized-robot`
    ##### General/Update Informations
    ##### Detailed Changes, Fixes & Improvements
    #### Container `misp-dockerized-postfix`
    ##### General/Update Informations
    Since 0.3.5 the postfix container is not more required.
    #### Container `misp-dockerized-proxy`
    ##### General/Update Informations
    ##### Detailed Changes, Fixes & Improvements
    #### Container `misp-dockerized-modules`
    ##### General/Update Informations
    - A new misp-modules container are used: 1.2-debian
    ##### Detailed Changes, Fixes & Improvements

---
+++ 1.0.0/.travis/tagging.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -exv
+
+DOCKER_REPO="$1"
+tag="$2"
+[ -z $(echo $tag|grep dev) ] && tag="$tag-dev"
+image_id=$(docker images --format "{{.Repository}}:{{.Tag}}:{{.ID}}"|grep $DOCKER_REPO:$tag|cut -d : -f 3|head -n 1;)
+image_tags=$(docker images --format "{{.Repository}}:{{.Tag}}:{{.ID}}"|grep $image_id|cut -d : -f 2;)
+for i in $image_tags
+do
+    k=$(echo $i|sed 's,-dev$,,')
+    docker tag $DOCKER_REPO:$i $DOCKER_REPO:$k; \
+done
