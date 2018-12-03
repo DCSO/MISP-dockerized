4b877a936eecb13ed7dda53bd5c0fb7e49676ba5
8ear <github@8ear.de>
2018-12-03 11:44:53 +0100

    Fix FOR_NEW_INSTALL.sh to deactivate make install in CI

--- FOR_NEW_INSTALL.sh
+++ FOR_NEW_INSTALL.sh
@@ -101,8 +101,9 @@ fi
         [ -L $PWD/current/config ] && echo "[OK] delete symlink 'current/config'" && rm $PWD/current/config
         echo "[OK] create symlink 'current/config' " && ln -s "../config" ./current/
 
-        echo "start installation..."
-        sleep 1
-        make -C current install
+
+        [ "$CI" == true ] || echo "start installation..."
+        [ "$CI" == true ] || sleep 1
+        [ "$CI" == true ] || make -C current install
 
     fi
