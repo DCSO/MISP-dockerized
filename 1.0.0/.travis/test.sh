40ff9b25203f485b2f302e49bfcd0da3cc8568a4
8ear <github@8ear.de>
2018-12-11 23:06:04 +0100

    Repair test

--- 1.0.0/.travis/test.sh
+++ 1.0.0/.travis/test.sh
@@ -2,6 +2,9 @@
 set -vx
 
 GIT_FOLDER="MISP-dockerized-testbench"
+HOSTNAME=$(cat ../docker-compose.override.yml |grep HOSTNAME |cut -d : -f 2|cut -d " " -f 2|head -1)
+
+
 
 # install dependencies for ALPINE!!!
 if [ ! -z "$(which apk)"  ]
@@ -11,6 +14,8 @@ then
     [ -z $(which bash) ] && echo "add bash..." && apk add --no-cache bash
     [ -z $(which make) ] && echo "add make..." && apk add --no-cache make 
     [ -z $(which sudo) ] && echo "add sudo..." && apk add --no-cache sudo 
+    [ -z $(which python3) ] && echo "add python3..." && apk add --no-cache python3 
+    [ -z $(which mysql) ] && echo "add python3..." && apk add --no-cache mysql-client 
     echo
 elif [ ! -z "$(which apt-get)"  ]
 then
@@ -22,38 +27,55 @@ then
     [ -z $(which make) ] && echo "add make..." && sudo apt-get -y install make 
     [ -z $(which python3) ] && echo "add python3..." && sudo apt-get -y install python3 
     [ -z $(which pip3) ] && echo "add python3-pip..." && sudo apt-get -y install python3-pip 
+    [ -z $(which mysql) ] && echo "add python3..." && sudo apt-get -y install  mysql-client
     
-    echo "add python3-venv..." && sudo apt-get -y install python3-venv 
-    echo "autoremove..." && sudo apt-get autoremove; 
-    echo "clean..." && sudo apt-get clean
+    echo "autoremove..." && sudo apt-get -y autoremove; 
+    echo "clean..." && sudo apt-get -y clean
     echo
 fi
 
 # clone the repository
 git clone https://github.com/DCSO/MISP-dockerized-testbench.git $GIT_FOLDER
 
-docker ps
+cd  $GIT_FOLDER
 
 # install python requirements
-#python3 -m venv venv
-#source venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 echo "pip3 install --no-cache-dir -r $GIT_FOLDER/requirements.txt" && pip3 install --no-cache-dir -r $GIT_FOLDER/requirements.txt
 
-# generate report foldergit clone htgit clone https://github.com/DCSO/MISP-dockerized-testbench.git $GIT_FOLDERtps://github.com/DCSO/MISP-dockerized-testbench.git $GIT_FOLDER
+# generate report folder
 [ -d reports ] || mkdir reports
 
 # Init MISP and create user
-    docker ps -a
-    sleep 60
-    docker ps -a
-[ -z $AUTH_KEY ] && export AUTH_KEY=$(docker exec misp-server bash -c "sudo -E /var/www/MISP/app/Console/cake userInit -q") && echo "new Auth_Key: $AUTH_KEY"
+sleep 60
+while true
+do
+    # copy auth_key
+    export AUTH_KEY=$(docker exec misp-server bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE -e "SELECT authkey FROM users;" | head -2|tail -1')
+    
+    # initial user if all is good auth_key is return
+    [ -z $AUTH_KEY  ] && export AUTH_KEY=$(docker exec misp-server bash -c "sudo -E /var/www/MISP/app/Console/cake userInit -q") && echo "new Auth_Key: $AUTH_KEY"
+    
+    # if user is initalized but mysql is not ready continue
+    [ "$AUTH_KEY" == "Script aborted: MISP instance already initialised." ] && continue
+    
+    # if the auth_key is save go out 
+    [ -z $AUTH_KEY ] || break
+    
+    # check status of misp-server
+    docker logs --tail 10 misp-server
+    
+    # wait 5 seconds
+    sleep 5
+done
 
 
 # generate settings.json
 cat << EOF > settings.json
 {
     "verify_cert": "False",
-    "url": "https://localhost",
+    "url": "https://${HOSTNAME}",
     "authkey": "${AUTH_KEY}",
     "basic_user": "admin@admin.test",
     "basic_password": "admin",
@@ -61,8 +83,15 @@ cat << EOF > settings.json
 }
 
 EOF
+cat settings.json
 
-echo "cat settings.json..." && cat settings.json
+
+
+echo "Add $HOSTNAME to 127.0.0.1 in /etc/hosts" && echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+
+
+
+
 
 # Run Tests
-echo "python3 $GIT_FOLDER/misp-testbench.py " && python3 $GIT_FOLDER/misp-testbench.py 
+echo "python3 $GIT_FOLDER/misp-testbench.py " && python3 misp-testbench.py 
