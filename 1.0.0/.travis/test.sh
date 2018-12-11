98bafdb6223fa1fee939431312eed6380ccd29e0
8ear <github@8ear.de>
2018-12-11 09:14:40 +0100

    Build in an sleep timer

--- 1.0.0/.travis/test.sh
+++ 1.0.0/.travis/test.sh
@@ -43,11 +43,11 @@ echo "pip3 install --no-cache-dir -r $GIT_FOLDER/requirements.txt" && pip3 insta
 [ -d reports ] || mkdir reports
 
 # Init MISP and create user
-docker ps -a
+    docker ps -a
+    sleep 60
+    docker ps -a
 [ -z $AUTH_KEY ] && export AUTH_KEY="$(docker exec misp-server bash -c 'sudo -E /var/www/MISP/app/Console/cake userInit -q')" && echo "new Auth_Key: $AUTH_KEY"
-docker exec misp-server bash -c 'sudo -E /var/www/MISP/app/Console/cake userInit -q'
-echo "new Auth_Key: $AUTH_KEY"
-docker ps
+
 
 # generate settings.json
 cat << EOF > settings.json
