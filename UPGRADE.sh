9546391db2db8059dbba29d6d1ed8cc48343c14f
8ear <github@8ear.de>
2018-12-17 14:29:25 +0100

    Change Upgrade

--- UPGRADE.sh
+++ UPGRADE.sh
@@ -9,14 +9,6 @@ set -ex
     [ -z "$CURRENT_CONTAINER" ] && echo "Sorry, no Upgrade is possible. The reason is there is no running misp-server. I exit now." && docker ps && exit
 
 
-function upgrade_to_1_0_0() {
-    echo "Upgrade from Version before 1.0.0"
-    # if current folder not exists execute install.sh script
-    
-
-
-}
-
 ##################      MAIN        #########################
 
 echo "#############################################################################"
@@ -37,33 +29,9 @@ then
         echo "no 'current' directory exists an direct upgrade is not possible."
         read -p "Do you upgrade from an version earlier than 1.0.0? [DEFAULT: $EARLIER_1_0_0]: " -ei $EARLIER_1_0_0  EARLIER_1_0_0
         [ "$EARLIER_1_0_0" == "no" ] && echo "There is a bug, please open a ticket on https://github.com/DCSO/MISP-dockerized/issues and report the Error. Now i will exit." && exit
+        [ "$EARLIER_1_0_0" == "yes" ] && echo "An Upgrade from an earlier version than 0.3.4 requires manual steps. See at https://dcso.github.io/MISP-dockerized-docs/ in the upgrade section"
     fi
-
-    [ "$CURRENT_CONTAINER" == "2.4.94" ] && INSTALLED_VERSION="0.3.4"
-    read -r -p "From which version do you upgrade?: " -ei "$INSTALLED_VERSION" INSTALLED_VERSION 
-        case $INSTALLED_VERSION in
-        0.3.4)
-            [ -L ./scripts ] || ln -s ./0.3.4/scripts ./scripts
-            make -C 0.3.4/ backup-all
-            [ -L ./scripts ] && rm ./scripts
-            break
-            ;;
-        0.2.0)
-            [ -L ./scripts ] || ln -s ./0.2.0/scripts ./scripts
-            make -C 0.2.0/ backup-all
-            break
-            ;;
-        0.1.2)
-            [ -L ./scripts ] || ln -s ./0.1.2/scripts ./scripts
-            make -C 0.1.2/ backup-all
-            break
-            ;;
-        *)
-            echo -e "\nplease choose only options from the text below!\n"
-            exit
-        ;;
-        esac
-
+ 
 
 
     # [2] make a backup
