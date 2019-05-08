3f8d4bed328140b9bbf262231f602b414572ccef
8ear <github@8ear.de>
2019-05-08 11:50:37 +0200

    Changed make delete* in Makefile - Changed make delete: should not delete volumes - Added make delete-volume - Added make delete-all

--- 1.2.0/scripts/delete_all_misp_from_host.sh
+++ 1.2.0/scripts/delete_all_misp_from_host.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
 #description     :This script remove all misp docker container, their volumes and the /opt/misp path.
 #==============================================================================
+STARTMSG="[DELETE]"
+#set -
 
-echo "This will remove all container, volumes and corresponding images."
+DELETE_CONTAINER="no"
+DELETE_IMAGES="no"
+DELETE_NETWORK="no"
+DELETE_NETWORK="no"
+DELETE_PRUNE="no"
+DELETE_VOLUMES="no"
+
+for i in $*
+    do
+        case "$i" in 
+            "--network")
+                DELETE_NETWORK="yes"
+                ;;
+            "--volumes")
+                DELETE_VOLUMES="yes"
+                ;;
+            "--container")
+                DELETE_CONTAINER="yes"
+                ;;
+            "--images")
+                DELETE_IMAGES="yes"
+                ;;
+            "--prune")
+                DELETE_PRUNE="yes"
+                ;;
+            *)
+            echo "$STARTMSG False Parameter."
+        esac
+    done
+
+
+echo "$STARTMSG This will remove container=$DELETE_CONTAINER, volumes=$DELETE_VOLUMES, network=$DELETE_NETWORK, images=$DELETE_IMAGES and dangling images=$DELETE_PRUNE ."
 [ CI == "true" ] || read -p "Are you sure? (y): " USER_GO
 [ CI == "true" ] && USER_GO="y"
 if [ "$USER_GO" == "y" ]; then
-    echo '### stop and remove all container ###'
-    docker rm -f $(docker ps -aqf name=misp*)
-    echo '### remove all volumes ###'
-    docker volume rm $(docker volume ls -qf name=misp*)
-    echo '### remove MISP images ###'
-    #docker image rm $(docker image ls --format '{{.Repository}}:{{.ID}}' | grep misp | sed 's/^[^:]*://g')
-    echo '### remove MISP Network'
-    docker network rm $(docker network ls --format '{{.Name}}:{{.ID}}' | grep misp | sed 's/^[^:]*://g')
+    
+    [ "$DELETE_CONTAINER" = "yes" ] && echo "$STARTMSG Stop and remove all misp-dockerized container"
+    [ "$DELETE_CONTAINER" = "yes" ] && docker rm -f $(docker ps -aqf name=misp-*)
+    
+    [ "$DELETE_VOLUMES" = "yes" ] && echo "$STARTMSG Remove all misp-dockerized volumes"
+    [ "$DELETE_VOLUMES" = "yes" ] && docker volume rm $(docker volume ls -qf name=misp-dockerized*)
+    
+    [ "$DELETE_IMAGES" = "yes" ] && echo "$STARTMSG Remove all misp-dockerized images ###"
+    [ "$DELETE_IMAGES" = "yes" ] && docker image rm $(docker image ls --format '{{.Repository}}:{{.ID}}' | grep misp-dockerized | sed 's/^[^:]*://g')
+    
+    [ "$DELETE_PRUNE" = "yes" ] && echo "$STARTMSG Remove all dangling"
+    [ "$DELETE_PRUNE" = "yes" ] && docker image rm $(docker image ls --filter "dangling=true" --quiet)
+
+    [ "$DELETE_NETWORK" = "yes" ] && echo "$STARTMSG Remove misp-dockerized Network"
+    [ "$DELETE_NETWORK" = "yes" ] && docker network rm $(docker network ls --format '{{.Name}}:{{.ID}}' | grep misp-dockerized | sed 's/^[^:]*://g')
 fi
