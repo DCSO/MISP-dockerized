3d11696c2523f53e42286319ac736a58932108c6
8ear <github@8ear.de>
2019-06-07 08:45:05 +0200

    Use central functions.sh

--- 1.2.0/scripts/requirements.sh
+++ 1.2.0/scripts/requirements.sh
@@ -10,7 +10,10 @@ DOCKER_SOCK="/var/run/docker.sock"
 
 
 # Load Variables from Configuration
-[ -e "$SCRIPTPATH/../.env" ] && source $SCRIPTPATH/../config/config.env
+[ -f "$SCRIPTPATH/../config/config.env" ] && source "$SCRIPTPATH/../config/config.env" && CONFIG_LOADED="true"
+
+# Load functions
+[ -f "$SCRIPTPATH/functions.sh" ] && source "$SCRIPTPATH/functions.sh"
 
 # to add options to the echo command
     echo () {
@@ -20,54 +23,20 @@ DOCKER_SOCK="/var/run/docker.sock"
 ####################    Start Script    ##############################
 
 #
-#   Check DOCKER
+#   Check Dependencies
 #
-    if [ -z "$(which docker)" ] 
-        then
-            STATUS="FAIL"
-            echo "[FAIL] Docker is not installed. \tPlease install it first!" 
-        else
-            echo "[OK] Docker is installed. \t\tOutput: $(docker -v)"   
-    fi
+func_check_docker
+func_check_git
 
-#
-#   Check GIT
-#
-    if [ -z "$(which git)" ] 
-        then
-            STATUS="FAIL"
-            echo -e "[FAIL] Git is not installed. \t\t\tPlease install it first!"
-        else
-            echo -e "[OK] Git is installed. \t\t\tOutput: $(git --version)"
-    fi
-
-#
-# CHECK required URLs
-#
 echo
-# check_URL: check a url with 2 parameters: URL_BASE=github.com & URL_PROTOCOL=http/https
-function check_URL(){
-    #set -xv
-    URL="$1"
-    [ "$USE_PROXY" == "yes" ] && PROXY=" -x $HTTP_PROXY"
-    OPTIONS="-vs --connect-timeout 60 -m 30 $PROXY"
-    COMMAND="$(curl $OPTIONS $URL 2>&1|grep 'Connected to')"
-    
-    if [ -z "$COMMAND" ]
-        then
-            echo "[WARN] Check: $URL"
-            echo "       Result: Connection not available."
-            #[ "$AUTOMATE_BUILD" == "true" ] || read -r -p "     continue with ENTER"  
-        else
-            echo "[OK]   Check: $URL"
-            echo "       Result: $COMMAND."
-    fi
-}
 
-check_URL https://misp.dcso.de
-check_URL https://dockerhub.dcso.de/v2/
-check_URL https://github.com/DCSO/misp-dockerized
-check_URL https://github.com/misp/misp
+#
+#   Check URLs
+#
+func_check_URL https://misp.dcso.de
+func_check_URL https://dockerhub.dcso.de/v2/
+func_check_URL https://github.com/DCSO/misp-dockerized
+func_check_URL https://github.com/misp/misp
 
 ###############################  USER CHECKS    #########################
 echo "" # Empty Line for a better overview.
@@ -113,65 +82,42 @@ echo
 [ ! -d ./config/pgp ]     && echo -n "Create config/pgp directory..."            && mkdir ./config/pgp         && echo "finished."
 [ ! -d ./backup ]         && echo -n "Create backup directory..."                && mkdir ./backup         && echo "finished."
 
-function check_folder(){
-    FOLDER="$1"
-    if [ ! -e "$FOLDER" ]
-            then
-                STATUS="FAIL"
-                echo "[FAIL] Can not create '$FOLDER' folder."
-            else
-                # user is in docker group
-                echo "[OK] Folder $FOLDER exists."
-                touch $FOLDER/test
-                if [ ! -e $FOLDER/test ]
-                    then
-                        STATUS="FAIL"
-                        echo "[FAIL] No write permissions in '$FOLDER'. Please ensure that user '${whoami}' has write permissions.'"
-                    else
-                        echo "[OK] Testfile in '$FOLDER' can be created."
-                        rm $FOLDER/test
-                fi
-        fi
-}
-
-check_folder "config"
-check_folder "config/ssl"
-check_folder "config/pgp"
-check_folder "config/smime"
-check_folder "backup"
-
-
+check_folder_write "config"
+check_folder_write "config/ssl"
+check_folder_write "config/pgp"
+check_folder_write "config/smime"
+check_folder_write "backup"
 
 ###############################  SSL CERT CHECKS    #########################
 echo
-if [ ! -f ./config/ssl/key.pem -a ! -f ./config/ssl/cert.pem ]; then
-    echo "[WARN] No SSL certificate found. We create a self-signed certificate in the volume."
-    echo "     To change the SSL certificate and private key later: "
-    echo "     1. Please save your certificate in:      $PWD/config/ssl/cert.pem"
-    echo "     2. Please save your private keyfile in:  $PWD/config/ssl/key.pem"
-    echo "     3. do:                         make config-ssl"
+if [ ! -f "./config/ssl/$SSL_KEY" ] && [ ! -f "./config/ssl/$SSL_CERT" ]; then
+    warn "[WARN] No SSL certificate found. We create a self-signed certificate in the volume."
+    warn "     To change the SSL certificate and private key later: "
+    warn "     1. Please save your certificate in:      $PWD/config/ssl/$SSL_CERT"
+    warn "     2. Please save your private keyfile in:  $PWD/config/ssl/$SSL_KEY"
+    warn "     3. do:                         make config-ssl"
     echo
     echo
 fi
 
 ###############################  SMIME CHECKS    #########################
 echo
-if [ ! -f ./config/smime/key.pem -a ! -f ./config/smime/cert.pem ]; then
-    echo "[WARN] No S/MIME certificate found."
-    echo "     1. Please save your certificate in:  $PWD/config/smime/cert.pem" 
-    echo "     2. Please save your private key  in:  $PWD/config/smime/key.pem"
-    echo "     3. Do:                        make config-smime"
+if [ ! -f "./config/smime/$SMIME_KEY" ] && [ ! -f "./config/smime/$SMIME_CERT" ]; then
+    warn "[WARN] No S/MIME certificate found."
+    warn "     1. Please save your certificate in:  $PWD/config/smime/$SMIME_CERT" 
+    warn "     2. Please save your private key  in:  $PWD/config/smime/$SMIME_KEY"
+    warn "     3. Do:                        make config-smime"
     echo
 fi
 
 ###############################  PGP CHECKS    #########################
 echo
-if [ ! -f ./config/pgp/private.key -a ! -f ./config/pgp/public.key ]; then
-    echo "[WARN] No PGP key found."
-    echo "     To replace the PGP public and private file later: "
-    echo "     1. Please save your public key in:      $PWD/config/pgp/public.key"
-    echo "     2. Please save your private key in:  $PWD/config/pgp/private.key"
-    echo "     3. Do:                         make config-pgp"
+if [ ! -f "./config/pgp/$PGP_PRIVATE_KEY" ] && [ ! -f "./config/pgp/$PGP_PUBLIC_KEY" ]; then
+    warn "[WARN] No PGP key found."
+    warn "     To replace the PGP public and private file later: "
+    warn "     1. Please save your public key in:      $PWD/config/pgp/$PGP_PUBLIC_KEY"
+    warn "     2. Please save your private key in:  $PWD/config/pgp/$PGP_PRIVATE_KEY"
+    warn "     3. Do:                         make config-pgp"
     echo
     echo
 fi
@@ -180,7 +126,7 @@ fi
 echo "End result:"
 if [ $STATUS == "FAIL" ]
     then
-        echo "[$STATUS] At least one error is occured."
+        error "[$STATUS] At least one error is occured."
         echo
         exit 1
     else
