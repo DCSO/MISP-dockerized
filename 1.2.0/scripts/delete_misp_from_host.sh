b77753f018a53888bb148f72922cbdd3c59d5ca3
8ear <github@8ear.de>
2019-06-11 09:35:28 +0200

    Fix delete_misp_from_host script

--- 1.2.0/scripts/delete_misp_from_host.sh
+++ 1.2.0/scripts/delete_misp_from_host.sh
@@ -55,23 +55,28 @@ do
 	shift
 done
 
-echo "This will remove MISP-dockerized container=$DELETE_CONTAINER, volumes=$DELETE_VOLUMES, network=$DELETE_NETWORK, images=$DELETE_IMAGES and dangling images=$DELETE_PRUNE?"
-[ "${CI-}" = "true" ] || read -r "Are you sure? (y): " USER_GO
+echo "This will remove MISP-dockerized container=$DELETE_CONTAINER, volumes=$DELETE_VOLUMES, network=$DELETE_NETWORK, images=$DELETE_IMAGES and dangling images=$DELETE_PRUNE? Are you sure? (y): "
+[ "${CI-}" = "true" ] || read -r USER_GO
 [ "${CI-}" = "true" ] && USER_GO="y"
 if [ "$USER_GO" = "y" ]; then
     
-    [ "$DELETE_CONTAINER" = "yes" ] && echo "$STARTMSG Stop and remove all misp-dockerized container"
+    [ "$DELETE_CONTAINER" = "yes" ] && echo "Stop and remove all misp-dockerized container"
+    # shellcheck disable=SC2046
     [ "$DELETE_CONTAINER" = "yes" ] && docker rm -f $(docker ps -aqf name=misp-*)
     
-    [ "$DELETE_VOLUMES" = "yes" ] && echo "$STARTMSG Remove all misp-dockerized volumes"
+    [ "$DELETE_VOLUMES" = "yes" ] && echo "Remove all misp-dockerized volumes"
+    # shellcheck disable=SC2046
     [ "$DELETE_VOLUMES" = "yes" ] && docker volume rm $(docker volume ls -qf name=misp-dockerized*)
     
-    [ "$DELETE_IMAGES" = "yes" ] && echo "$STARTMSG Remove all misp-dockerized images ###"
+    [ "$DELETE_IMAGES" = "yes" ] && echo "Remove all misp-dockerized images ###"
+    # shellcheck disable=SC2046
     [ "$DELETE_IMAGES" = "yes" ] && docker image rm $(docker image ls --format '{{.Repository}}:{{.ID}}' | grep misp-dockerized | sed 's/^[^:]*://g')
     
-    [ "$DELETE_PRUNE" = "yes" ] && echo "$STARTMSG Remove all dangling"
+    [ "$DELETE_PRUNE" = "yes" ] && echo "Remove all dangling"
+    # shellcheck disable=SC2046
     [ "$DELETE_PRUNE" = "yes" ] && docker image rm $(docker image ls --filter "dangling=true" --quiet)
 
-    [ "$DELETE_NETWORK" = "yes" ] && echo "$STARTMSG Remove misp-dockerized Network"
+    [ "$DELETE_NETWORK" = "yes" ] && echo "Remove misp-dockerized Network"
+    # shellcheck disable=SC2046
     [ "$DELETE_NETWORK" = "yes" ] && docker network rm $(docker network ls --format '{{.Name}}:{{.ID}}' | grep misp-dockerized | sed 's/^[^:]*://g')
 fi
