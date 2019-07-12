e7533bf987703f7805e37c57d5bb449fafc5f23f
8ear <github@8ear.de>
2019-07-12 12:23:23 +0200

    Fix MISP delete script

--- 1.2.0/scripts/delete_misp_from_host.sh
+++ 1.2.0/scripts/delete_misp_from_host.sh
@@ -11,10 +11,8 @@ STARTMSG="${Light_Green}[DELETE]${NC}"
 DELETE_CONTAINER="no"
 DELETE_IMAGES="no"
 DELETE_NETWORK="no"
-DELETE_NETWORK="no"
 DELETE_PRUNE="no"
 DELETE_VOLUMES="no"
-
 
 # Functions
 echo (){
@@ -57,7 +55,15 @@ done
 
 echo "This will remove MISP-dockerized container=$DELETE_CONTAINER, volumes=$DELETE_VOLUMES, network=$DELETE_NETWORK, images=$DELETE_IMAGES and dangling images=$DELETE_PRUNE? Are you sure? (y): "
 [ "${CI-}" = "true" ] || read -r USER_GO
-[ "${CI-}" = "true" ] && USER_GO="y"
+
+if [ "${CI-}" = "true" ];then
+    USER_GO="y"
+    DELETE_CONTAINER="yes"
+    DELETE_NETWORK="yes"
+    DELETE_PRUNE="yes"
+    DELETE_VOLUMES="yes"
+fi
+
 if [ "$USER_GO" = "y" ]; then
     
     [ "$DELETE_CONTAINER" = "yes" ] && echo "Stop and remove all misp-dockerized container"
@@ -78,5 +84,5 @@ if [ "$USER_GO" = "y" ]; then
 
     [ "$DELETE_NETWORK" = "yes" ] && echo "Remove misp-dockerized Network"
     # shellcheck disable=SC2046
-    [ "$DELETE_NETWORK" = "yes" ] && docker network rm $(docker network ls --format '{{.Name}}:{{.ID}}' | grep misp-dockerized | sed 's/^[^:]*://g')
+    [ "$DELETE_NETWORK" = "yes" ] && docker network rm $(docker network ls --format '{{.Name}}:{{.ID}}' | grep misp-backend | sed 's/^[^:]*://g')
 fi
