aec8a47a88d852fdb0c338062475fdc19bf3a808
8ear <github@8ear.de>
2019-07-12 10:17:47 +0200

    Improve test.sh

--- 1.2.0/scripts/test.sh
+++ 1.2.0/scripts/test.sh
@@ -8,18 +8,13 @@ Light_Green='\033[1;32m'
 STARTMSG="${Light_Green}[TEST]${NC}"
 AUTH_KEY=""
 
-REPORT_FOLDER="$PWD"
+REPORT_FOLDER="$PWD../.ci"
 REPORT_FILE="$REPORT_FOLDER/reports/*.xml"
 
 # Functions
 echo (){
     command echo "$STARTMSG $*"
 }
-
-# Environment Variables
-GIT_FOLDER=${GIT_FOLDER:-"/srv/MISP-dockerized-testbench"}
-MISP_FQDN=${MISP_FQDN:-"$(grep MISP_FQDN /srv/MISP-dockerized/config/config.env |cut -d = -f 2)"}
-MISP_BASEURL=${MISP_BASEURL:-"https://$MISP_FQDN"}
 
 
 #
@@ -39,27 +34,10 @@ do
     #wait x seconds
     echo "$(date +%T) -  wait until misp-server is ready. sleep $SLEEP_TIMER seconds..."
     docker logs --tail 10 misp-server
+    command echo 
     sleep "$SLEEP_TIMER"
-    SLEEP_TIMER="(( $SLEEP_TIMER + 5))"
-done
-
-# Init MISP and create user
-while true
-do
-    # copy auth_key
-    export AUTH_KEY="$(docker exec misp-server bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE -e "SELECT authkey FROM users;" | head -2|tail -1')"
-    
-    # initial user if all is good auth_key is return
-    [ -z "$AUTH_KEY"  ] && export AUTH_KEY="$(docker exec misp-server bash -c "sudo -E /var/www/MISP/app/Console/cake userInit -q")" && echo "new Auth_Key: $AUTH_KEY"
-    
-    # if user is initalized but mysql is not ready continue
-    [ "$AUTH_KEY" = "Script aborted: MISP instance already initialised." ] && continue
-    
-    # if the auth_key is save go out 
-    [ -z "$AUTH_KEY" ] || break
-
-    # wait 5 seconds
-    echo "$(date +%T) -  wait until the test script get the authentication key." && sleep 5
+    # shellcheck disable=SC2004
+    SLEEP_TIMER=$(( $SLEEP_TIMER + 5))
 done
 
 echo "################		Start Tests		###########################"
