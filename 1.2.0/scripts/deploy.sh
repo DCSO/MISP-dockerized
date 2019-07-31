92915c4beb1827cef2eed238c358eae43e5dab45
8ear <github@8ear.de>
2019-07-31 12:50:35 +0200

    Fix deploy script

--- 1.2.0/scripts/deploy.sh
+++ 1.2.0/scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+DOCKER_REGISTRY="$(grep DOCKER_REGISTRY "$PWD"/config/config.env|cut -d = -f 2)"
+ROBOT_CONTAINER_TAG="$(grep ROBOT_CONTAINER_TAG "$PWD"/config/config.env|cut -d = -f 2)"
 
+# shellcheck disable=SC2086
 docker run \
-	    --name misp-robot-init \
-		--rm \
-	    	--network="host" \
-	    	-v "$PWD":/srv/MISP-dockerized \
+	    --name misp-robot-init --rm --network="host" \
+		-v "$PWD":/srv/MISP-dockerized \
 		-v /var/run/docker.sock:/var/run/docker.sock:ro \
-		"$(grep DOCKER_REGISTRY "$PWD"/config/config.env|cut -d = -f 2)"/misp-dockerized-robot:\
-			"$(grep ROBOT_CONTAINER_TAG "$PWD"/config/config.env|cut -d = -f 2)" \
+		"$DOCKER_REGISTRY"/misp-dockerized-robot:$ROBOT_CONTAINER_TAG \
 				bash -c "docker-compose -f /srv/MISP-dockerized/docker-compose.yml \
 					-f /srv/MISP-dockerized/docker-compose.override.yml up -d "
 # Copy SSL
