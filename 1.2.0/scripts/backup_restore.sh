edac366796b1b05274fe951d30dca514bb3877ec
8ear <github@8ear.de>
2019-08-05 18:51:20 +0200

    Improve backup-restore script

--- 1.2.0/scripts/backup_restore.sh
+++ 1.2.0/scripts/backup_restore.sh
@@ -27,9 +27,10 @@ else
       "$DOCKER_REGISTRY"/misp-dockerized-robot:"$ROBOT_CONTAINER_TAG" sh -c "docker-compose -f /srv/MISP-dockerized/current/docker-compose.yml -f /srv/MISP-dockerized/current/docker-compose.override.yml up -d misp-robot"
 fi
 
-
 # If you want to restore first install environment
+  echo "Install standard deployment ..."
   [ "${1-}" = "restore" ] && make install && sleep 2
+
 # Start script
-  echo "Start backup_restore script..."
+  echo "Start backup_restore script ..."
   docker exec -ti misp-robot sh -c "/srv/scripts/backup_restore.sh $*"
